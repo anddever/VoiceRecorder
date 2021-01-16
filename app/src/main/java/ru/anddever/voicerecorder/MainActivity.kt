ba16856adc922190e36fd8badc074fe13996075e
anddever <anddever@gmail.com>
2021-01-16 20:32:01 +0700

    Create RecordService

--- app/src/main/java/ru/anddever/voicerecorder/MainActivity.kt
+++ app/src/main/java/ru/anddever/voicerecorder/MainActivity.kt
@@ -1,5 +1,7 @@
 package ru.anddever.voicerecorder
 
+import android.app.ActivityManager
+import android.content.Context
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.Navigation
@@ -7,11 +9,22 @@ import androidx.navigation.ui.NavigationUI
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         NavigationUI.setupWithNavController(bottom_navigation,
             Navigation.findNavController(this, R.id.nav_host_fragment_container))
+    }
+
+    fun isServiceRunning(): Boolean {
+        val manager = getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
+        for (service in manager.getRunningServices(Int.MAX_VALUE)) {
+            if ("ru.anddever.voicerecorder.record.RecordService" == service.service.className) {
+                return true
+            }
+        }
+        return false
     }
 }
